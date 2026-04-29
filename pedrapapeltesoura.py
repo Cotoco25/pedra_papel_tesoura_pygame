bc5c2cb4e66c8cd36ac58ceb409728fd44951e2c
Cotoco25 <guilhermerrtostes@gmail.com>
2026-04-29 14:50:24 -0300

    dsadsa

--- pedrapapeltesoura.py
+++ pedrapapeltesoura.py
@@ -13,11 +13,11 @@ clock=time.Clock()
 soma = 0
 soma_adv = 0
 timer = 0
-
+escolha = ""
 escolha_ia = ["pedra", "papel", "tesoura"]
 adversario = random.choice(escolha_ia)
 
-escolha = input("escolha: ")
+#escolha = input("escolha: ")
 
 pedra_img = image.load("pedra.png")
 pedra = transform.scale(pedra_img, (300,300))
@@ -31,21 +31,23 @@ tesoura = transform.scale(tesoura_img, (310,190))
 background_color = (112, 128, 144)
 fonte = font.Font("fonte.ttf", 40)
 chique = font.Font("fontechique.ttf", 60)
-chique_pequena = font.Font("fontechique.ttf", 25)
+chique_pequena = font.Font("fontechique.ttf", 35)
 
 def ia_pedra(escolha):
-    global adversario
+    global adversario, timer, soma, soma_adv
     if escolha == "pedra":
         result_empate = chique.render(f"Empate", True, (255,255,0))
         escolha_adversario = chique.render(f"A IA escolheu {adversario}", True, (255,255,0))
         window.blit(result_empate, (600,200))
         window.blit(escolha_adversario, (600,300))
+    
     elif escolha == "papel":
         result_vit = chique.render(f"VOCE GANHOU!!!", True, (255,255,0))
         escolha_adversario = chique.render(f"A IA escolheu {adversario}", True, (255,255,0))
         window.blit(result_vit, (600,200))
         window.blit(escolha_adversario, (600,300))
         soma +=1
+    
     elif escolha == "tesoura":
         result_derrota = chique.render(f"voce perdeu :(", True, (255,255,0))
         escolha_adversario = chique.render(f"A IA escolheu {adversario}", True, (255,255,0))
@@ -54,8 +56,9 @@ def ia_pedra(escolha):
         soma_adv +=1
 
 
+
 def ia_papel(escolha):
-    global adversario
+    global adversario, timer, soma, soma_adv
     if escolha == "pedra":
         result_derrota = chique.render(f"voce perdeu :(", True, (255,255,0))
         escolha_adversario = chique.render(f"A IA escolheu {adversario}", True, (255,255,0))
@@ -79,7 +82,7 @@ def ia_papel(escolha):
 
 
 def ia_tesoura(escolha):
-    global adversario
+    global adversario, timer, soma, soma_adv
     if escolha == "pedra":
         result_vit = chique.render(f"VOCE GANHOU!!!", True, (255,255,0))
         escolha_adversario = chique.render(f"A IA escolheu {adversario}", True, (255,255,0))
@@ -99,6 +102,12 @@ def ia_tesoura(escolha):
         window.blit(escolha_adversario, (600,300))
 
 
+def reiniciar_jogo():
+    global adversario, timer
+    adversario = random.choice(escolha_ia)
+    timer = 240
+
+
 while running:
     clock.tick(60)
     key_pressed = key.get_pressed()
@@ -110,12 +119,40 @@ while running:
             quit()
             running = False
             sys.exit()
-    
-    choice_ia = chique.render(f"{adversario}", True, (255,255,0))
+
+  
+
+        if ev.type == MOUSEBUTTONDOWN:
+                if 100<mouse_x<350 and 300<mouse_y<600:
+                    if ev.button == 1:
+                        escolha = "pedra"
+                        timer = 240
+
+            
+        if ev.type == MOUSEBUTTONDOWN:
+                if 500<mouse_x<850 and 300<mouse_y<600:
+                    if ev.button == 1:
+                        escolha = "papel"
+                        timer = 240
+
+        
+        if ev.type == MOUSEBUTTONDOWN:
+                if 900<mouse_x<1250 and 300<mouse_y<600:
+                    if ev.button == 1:
+                        escolha = "tesoura"
+                        timer = 240
+
+    choice_ia = chique.render(f"ia escolheu {adversario}", True, (255,255,0))
     window.blit(choice_ia, (600,0))
 
     texto_base = chique.render(f"escolha:", True, (255,255,255))
     window.blit(texto_base, (600,100))
+
+    pontos = chique_pequena.render(f"Pontos: {soma}", True, (255,255,255))
+    window.blit(pontos, (50,100))
+
+    pontos_ia = chique_pequena.render(f"Pontos ia: {soma_adv}", True, (255,255,255))
+    window.blit(pontos_ia, (50,200))
 
     draw.rect(window, (120, 120, 120), (100, 300,350,300))
     draw.rect(window, (120, 120, 120), (500, 300,350,300))
@@ -129,21 +166,15 @@ while running:
     window.blit(papel, (530,300))
     window.blit(tesoura, (930,350))
 
-    if escolha_ia == pedra:
-        timer = 240
+    if adversario == "pedra":
         ia_pedra(escolha)
-    
-    if escolha_ia == papel:
-        timer = 240
+    elif adversario == "papel":
         ia_papel(escolha)
-    
-    if escolha_ia == tesoura:
-        timer = 240
+    elif adversario == "tesoura":
         ia_tesoura(escolha)
 
 
-
-
+    
 
 
     display.update()
